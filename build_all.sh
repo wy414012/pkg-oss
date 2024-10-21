9dbfb5ce350db8afd0f61507d6f9393243680b12
Follow the wind <36281137+wy414012@users.noreply.github.com>
2024-10-21 20:00:43 +0800

    添加清理构建缓存

--- build_all.sh
+++ build_all.sh
@@ -21,12 +21,15 @@ cd /host/pkg-oss/debian
 
 make base module-auth-spnego module-lua module-njs module-image-filter module-geoip2 module-geoip module-subs-filter module-rtmp module-set-misc module-perl module-fips-check module-brotli module-xslt module-ndk module-headers-more module-otel module-encrypted-session module-opentracing
 
-# 将生成的包指定到目录存储
+# 清理构建完毕的缓存
+cd /host/pkg-oss/debian/ && \
+make clean
+
+# 将生成的包同步指定到目录存储
 mkdir -p /host/pkg-oss/pkg-output/
 
 rsync -avz /host/pkg-oss/../nginx* /host/pkg-oss/pkg-output/
 
-# 删除构建目录
-# 由于是映射本地目录所以不用删除构建缓存 只需要执行删除依赖root缓存rm -rf /host/pkg-oss/contrib
+# 删除依赖目录
 rm -rf /root/boringssl 
 rm -rf /root/ngx-fancyindex
